4d1d226b65c2a4e86d8c339f422c4dcadb405563
Maisho20 <bahajazz70@gmail.com>
2024-05-22 00:42:38 +0700

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,20 @@
 const Footer = () => {
   return (
-    <footer class="bg-gray-900">
-      <div class="mx-auto w-full min-w-screen p-4 py-6 lg:py-8">
-        <div class="flex justify-center">
-          <div class="mb-6 md:mb-0">
-            <h1 class="self-center text-2xl font-semibold whitespace-nowrap text-white mb-3">
+    <footer className="bg-gray-900">
+      <div className="mx-auto w-full min-w-screen p-4 py-6 lg:py-8">
+        <div className="flex justify-center">
+          <div className="mb-6 md:mb-0">
+            <h1 className="self-center text-2xl font-semibold whitespace-nowrap text-white mb-3">
               Contacs
             </h1>
           </div>
         </div>
 
         {/* icons */}
-        <div class="flex justify-center sm:mt-0">
-          <a href="#" class="text-gray-500 hover:text-white">
+        <div className="flex justify-center sm:mt-0">
+          <a href="#" className="text-gray-500 hover:text-white">
             <svg
-              class="w-4 h-4"
+              className="w-4 h-4"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -26,12 +26,12 @@ const Footer = () => {
                 clip-rule="evenodd"
               />
             </svg>
-            <span class="sr-only">Facebook page</span>
+            <span className="sr-only">Facebook page</span>
           </a>
 
-          <a href="#" class="text-gray-500 hover:text-white ms-5">
+          <a href="#" className="text-gray-500 hover:text-white ms-5">
             <svg
-              class="w-4 h-4"
+              className="w-4 h-4"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -39,12 +39,12 @@ const Footer = () => {
             >
               <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
             </svg>
-            <span class="sr-only">Discord community</span>
+            <span className="sr-only">Discord community</span>
           </a>
 
-          <a href="#" class="text-gray-500 hover:text-white ms-5">
+          <a href="#" className="text-gray-500 hover:text-white ms-5">
             <svg
-              class="w-4 h-4"
+              className="w-4 h-4"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -56,12 +56,12 @@ const Footer = () => {
                 clip-rule="evenodd"
               />
             </svg>
-            <span class="sr-only">Twitter page</span>
+            <span className="sr-only">Twitter page</span>
           </a>
 
-          <a href="#" class="text-gray-500 hover:text-white ms-5">
+          <a href="#" className="text-gray-500 hover:text-white ms-5">
             <svg
-              class="w-4 h-4"
+              className="w-4 h-4"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -73,12 +73,12 @@ const Footer = () => {
                 clip-rule="evenodd"
               />
             </svg>
-            <span class="sr-only">GitHub account</span>
+            <span className="sr-only">GitHub account</span>
           </a>
 
-          <a href="#" class="text-gray-500 hover:text-white ms-5">
+          <a href="#" className="text-gray-500 hover:text-white ms-5">
             <svg
-              class="w-4 h-4"
+              className="w-4 h-4"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -90,15 +90,15 @@ const Footer = () => {
                 clip-rule="evenodd"
               />
             </svg>
-            <span class="sr-only">Dribbble account</span>
+            <span className="sr-only">Dribbble account</span>
           </a>
         </div>
         {/* end icons */}
 
-        <hr class="my-6 sm:mx-auto border-gray-700 lg:my-8" />
+        <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8" />
 
-        <div class="flex items-center justify-center">
-          <span class="text-sm sm:text-center text-gray-400">
+        <div className="flex items-center justify-center">
+          <span className="text-sm sm:text-center text-gray-400">
             © 2024 <b>Sabbaha Naufal</b> . All Rights Reserved.
           </span>
         </div>
